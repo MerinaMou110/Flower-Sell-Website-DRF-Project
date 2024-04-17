d27570c2de8b4a36db4d63460964e15cd2a27480
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2024-04-17 22:48:09 +0600

    add CSRF

--- Happy_Flower/settings.py
+++ Happy_Flower/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-66ukc6dk529)#tad^n^!=(%cxr_-c16w-f7g4499vz3np!mlz+"
+
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://flower-sell-website-drf-project.onrender.com','https://*.127.0.0.1'] 
 
 
 # Application definition
