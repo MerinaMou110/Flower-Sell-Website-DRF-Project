6f2aede7a7dcd42dd740c103720b4e5d4b2aaab2
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2024-04-18 20:26:05 +0600

    make change

--- Happy_Flower/settings.py
+++ Happy_Flower/settings.py
@@ -63,10 +63,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5501',
-    # Add any other allowed origins as needed
-]
 
 CSRF_TRUSTED_ORIGINS = ['https://flower-sell-website-drf-project.onrender.com']
 ROOT_URLCONF = "Happy_Flower.urls"
